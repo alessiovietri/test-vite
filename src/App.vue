d560ca1829ca7e167e0dfe9cc7233ae0e54093a1
Alessio Vietri <info@alessiovietri.it>
2023-02-01 13:07:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import MainTitle from './components/MainTitle.vue';
+
+export default {
+    components: {
+        MainTitle
+    },
+    data() {
+        return {
+            count: 0,
+        }
+    }
+}
+</script>
+
+<template>
+    <MainTitle />
+
+    <button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+  color: red;
+}
+</style>
---
+++ src/components/MainTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            message: 'Ciao Vite dal componente!'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>
+        {{ message }}
+    </h1>
+</template>
+
+<style scoped>
+h1 {
+    color: green;
+}
+</style>
